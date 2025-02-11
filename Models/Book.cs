7e521dde352648ab5e7b97d7d50e0ce54be54167
Kristijonas-BMK <kristijonas.januskevicius@bmk.lt>
2025-02-11 11:57:37 +0200

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+namespace CourseraAPIProject {
+    public class Book {
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
